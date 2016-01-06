c604aa9bc61a9f9313f308f8ee243d0d47de28ab
Davy Peter Braun <hello@davybraun.com>
2016-01-06 20:11:01 +0100

    Implement and test rendering of voting buttons.

---
+++ src/components/Voting.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+export default class Voting extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getPair() {
+    return this.props.pair || [];
+  }
+
+  render() {
+    return (
+      <div className="voting">
+        {this.getPair().map(entry =>
+          <button key={entry}>
+            <h1>{entry}</h1>
+          </button>
+        )}
+      </div>
+    );
+  }
+}
