54445c9cc34b0ee42908952dd6cf80faadeb76e3
Davy Peter Braun <hello@davybraun.com>
2016-01-06 20:17:19 +0100

    Implement and test that Voting calls a callback props on click.

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -5,7 +5,8 @@ import {expect} from 'chai';
 
 const {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithTag
+  scryRenderedDOMComponentsWithTag,
+  Simulate
 } = ReactTestUtils;
 
 describe('Voting', () => {
@@ -18,5 +19,19 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              vote={vote} />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
   });
 });
