ccee6617361fcc0c17185dc7e2fc218438792d70
Davy Peter Braun <hello@davybraun.com>
2016-01-06 21:10:49 +0100

    Ensure user cannot vote twice, and display voted entry as label in button.

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -9,13 +9,25 @@ export default class Voting extends Component {
     return this.props.pair || [];
   }
 
+  isDisabled() {
+    return !!this.props.hasVoted;
+  }
+
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }
+
   render() {
     return (
       <div className="voting">
         {this.getPair().map(entry =>
           <button key={entry}
+                  disabled={this.isDisabled()}
                   onClick={() => this.props.vote(entry)}>
             <h1>{entry}</h1>
+            {this.hasVotedFor(entry) ?
+              <div className="label">Voted</div> :
+              null}
           </button>
         )}
       </div>
