3af020f450f41478ceb081be92bfda3e570332ca
Davy Peter Braun <hello@davybraun.com>
2016-01-08 07:39:21 +0100

    Create basic Results component.

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router'
 import Voting from './components/Voting';
 import App from './components/App';
+import Results from './components/Results';
 
 ReactDOM.render(
   <Router>
     <Route history={browserHistory} component={App}>
+      <Route path="/results" component={Results} />
       <Route path="/" component={Voting} />
     </Route>
   </Router>,
