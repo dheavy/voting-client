81e7093ddfe14df232433463e46b44bb53fbe641
Davy Peter Braun <hello@davybraun.com>
2016-01-11 21:05:24 +0100

    Set up Redux store.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router'
+import Results from './components/Results';
 import Voting from './components/Voting';
 import App from './components/App';
-import Results from './components/Results';
+import {createStore} from 'redux';
+import ReactDOM from 'react-dom';
+import reducer from './reducer';
+import React from 'react';
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+})
 
 ReactDOM.render(
   <Router>
